baf49740c444638df4c92810663a4fb19c95461d
Shujaat Azim <shujaat.azim@gmail.com>
2020-09-20 19:05:58 -0400

    Added ability to create courses, and buil tout the form to do so. Also changed come layout around, but it's still ugly af. Going to add ability to edit courses and students. Also maybe some validations and more fields for courses in he future.

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,4 +1,36 @@
 class CoursesController < ApplicationController
+
   def index
+    @courses = Course.all
   end
+
+  def show
+    @course = Course.find(params[:id])
+  end
+
+  def new
+    @course = Course.new
+  end
+
+  def create
+    @course = Course.create(params.require(:course).permit(:name))
+    redirect_to courses_path
+  end
+
+  def edit
+    @course = Course.find(params[:id])
+  end
+
+  def update
+    @course = Course.find(params[:id])
+    @course.update(params.require(:course).permit(:name))
+    redirect_to course_path(@course)
+  end
+
+  def destroy
+    @course = Course.find(params[:id])
+    @course.destroy
+    redirect_to courses_path
+  end
+
 end
